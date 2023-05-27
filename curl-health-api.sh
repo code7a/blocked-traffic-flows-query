9856c6d444343bbfec764ac77fe52972cef66032
code7a <115498258+code7a@users.noreply.github.com>
2023-05-26 22:19:53 -0400

    health check

--- curl-health-api.sh
+++ curl-health-api.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #curl health api
-fqdn=$(cat /usr/share/nginx/html/config.php |grep fqdn|cut -d "'" -f2)
-port=$(cat /usr/share/nginx/html/config.php |grep port|cut -d "'" -f2)
+fqdn=$(cat /usr/share/nginx/html/config.php|grep fqdn|cut -d "'" -f2)
+port=$(cat /usr/share/nginx/html/config.php|grep port|cut -d "'" -f2)
 user=$(cat /usr/share/nginx/html/config.php|grep user|cut -d "'" -f2)
 key=$(cat /usr/share/nginx/html/config.php|grep key|cut -d "'" -f2)
 curl https://$user:$key@$fqdn:$port/api/v2/health
