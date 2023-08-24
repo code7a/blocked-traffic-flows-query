4b96cf0586713055a3302ec60e8147a88aecc0cd
code7a <115498258+code7a@users.noreply.github.com>
2023-08-24 15:40:36 -0400

    increase memory limit

--- index.php
+++ index.php
@@ -209,6 +209,7 @@
             <?php
                 ini_set('display_errors', '1');
                 ini_set('display_startup_errors', '1');
+                ini_set("memory_limit", "1024M");
                 error_reporting(E_ALL);
                 if(isset($_POST['submit'])){blocked_traffic_flows_query();}
             ?>
